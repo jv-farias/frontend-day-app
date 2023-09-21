d9ad704c9364b7a23ef67ab9ddb7fbe45f5d9742
João Vitor R. Farias <84153149+jv-farias@users.noreply.github.com>
2023-09-21 03:57:13 +0000

    exclusão de linhas desnecessárias do javascript

--- js/main.js
+++ js/main.js
@@ -4,11 +4,3 @@ import { htmlGerado2 } from '../dist/cardsHtml.js';
 import { btnFrontEnd } from '../dist/botoesTrilhas.js';
 import { btnComunidade } from '../dist/botoesTrilhas.js';
 import { btnConvida } from '../dist/botoesTrilhas.js';
-
-htmlGerado();
-htmlGerado1();
-htmlGerado2();
-btnFrontEnd();
-btnComunidade();
-btnConvida();
-
