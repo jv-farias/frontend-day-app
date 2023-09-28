1f264618ddd66d2d39ba899df84f7638f787dc60
João Vitor R. Farias <jvmnc232@gmail.com>
2023-09-28 01:32:54 +0000

    update card

--- js/main.js
+++ js/main.js
@@ -6,3 +6,28 @@
 // import { btnConvida } from '../dist/botoesTrilhas.js';
 import { htmlGeradoAoVivo } from '../dist/cardsHtml.js';
 
+document.addEventListener("DOMContentLoaded", function () {
+    function atualizarCardsAoVivo() {
+      const now = moment();
+      const currentTime = now.format("HH:mm");
+  
+      const cardsAoVivo = document.querySelectorAll(".cards-palestrante-content, .cards-topicos-content");
+      cardsAoVivo.forEach((card) => {
+        const start = card.getAttribute("data-start");
+        const end = card.getAttribute("data-end");
+  
+        if (currentTime >= start && currentTime < end) {
+          card.style.display = "block"; // Exibe o card
+        } else {
+          card.style.display = "none"; // Oculta o card
+        }
+      });
+    }
+  
+    atualizarCardsAoVivo(); // Chama a função para atualizar os cards ao carregar a página
+  
+    // Define um intervalo para verificar e atualizar os cards a cada minuto
+    setInterval(atualizarCardsAoVivo, 60000); // Atualiza a cada minuto (60 segundos)
+  });
+
+  
