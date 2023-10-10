bd06b848476ac3fb2d43b55ec6cc64f0bf60b373
Rodrigo Neto <134714036+RodrigoNet0@users.noreply.github.com>
2023-10-10 18:37:22 +0000

    bug do enter resolvido,porém ainda persisti na aba geral

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ import { cardsProcessadosFrontEnd } from "./templateFrontEnd.js";
 function debounce(func, delay) {
   let timer; // Variável para armazenar o identificador do temporizador
 
-  return function() {
+  return function () {
     clearTimeout(timer); // Limpa o temporizador se já estiver definido
 
     timer = setTimeout(() => {
@@ -22,58 +22,64 @@ function debounce(func, delay) {
 // Obtém a barra de pesquisa
 const searchBar = document.getElementById("search-box-input");
 
-function search(){
-  
-      const searchTerm = searchBar.value.toLowerCase();
+function search() {
 
-      // Obtém todos os elementos com classe "cards-cronograma-content" (cards de palestrantes)
-      const cardsPalestrantes = document.querySelectorAll(".cards-cronograma-content");
-    
-      // Itera sobre todos os elementos de cards de palestrantes
-      cardsPalestrantes.forEach((element) => {
-        const nomeElement = element.querySelector(".nome");
-        const assuntoElement = element.querySelector(".assunto");
-    
-        if (nomeElement && assuntoElement) {
-          const nome = nomeElement.textContent.toLowerCase();
-          const assunto = assuntoElement.textContent.toLowerCase();
-    
-          // Verifica se o elemento corresponde à pesquisa
-          if (nome.includes(searchTerm) || assunto.includes(searchTerm) || searchTerm === "") {
-            // Se corresponder (ou se a pesquisa estiver vazia), exibe o elemento
-            element.style.display = "flex";
-          } else {
-            // Caso contrário, oculta o elemento
-            element.style.display = "none";
-          }
-        }
-      
-        
-      });
-      
-      // Obtém todos os elementos com classe "topicos-cronograma-content" (tópicos)
-      const topicos = document.querySelectorAll(".topicos-cronograma-content");
-      
-      // Itera sobre todos os elementos de tópicos
-      topicos.forEach((element) => {
-        // Verifica se o elemento contém informações relevantes para a pesquisa
-        const nomeElement = element.querySelector(".nome-topico");
-      
-        if (nomeElement) {
-          const nome = nomeElement.textContent.toLowerCase();
-      
-          // Verifica se o elemento corresponde à pesquisa
-          if (nome.includes(searchTerm) || searchTerm === "") {
-            // Se corresponder (ou se a pesquisa estiver vazia), exibe o elemento
-            element.style.display = "flex";
-          } else {
-            // Caso contrário, oculta o elemento
-            element.style.display = "none";
-          }
-        }
-      });
+  const searchTerm = searchBar.value.toLowerCase();
+
+  // Obtém todos os elementos com classe "cards-cronograma-content" (cards de palestrantes)
+  const cardsPalestrantes = document.querySelectorAll(".cards-cronograma-content");
+
+  // Itera sobre todos os elementos de cards de palestrantes
+  cardsPalestrantes.forEach((element) => {
+    const nomeElement = element.querySelector(".nome");
+    const assuntoElement = element.querySelector(".assunto");
+
+    if (nomeElement && assuntoElement) {
+      const nome = nomeElement.textContent.toLowerCase();
+      const assunto = assuntoElement.textContent.toLowerCase();
+
+      // Verifica se o elemento corresponde à pesquisa
+      if (nome.includes(searchTerm) || assunto.includes(searchTerm) || searchTerm === "") {
+        // Se corresponder (ou se a pesquisa estiver vazia), exibe o elemento
+        element.style.display = "flex";
+      } else {
+        // Caso contrário, oculta o elemento
+        element.style.display = "none";
+      }
+    }
+
+
+  });
+
+  // Obtém todos os elementos com classe "topicos-cronograma-content" (tópicos)
+  const topicos = document.querySelectorAll(".topicos-cronograma-content");
+
+  // Itera sobre todos os elementos de tópicos
+  topicos.forEach((element) => {
+    // Verifica se o elemento contém informações relevantes para a pesquisa
+    const nomeElement = element.querySelector(".nome-topico");
+
+    if (nomeElement) {
+      const nome = nomeElement.textContent.toLowerCase();
+
+      // Verifica se o elemento corresponde à pesquisa
+      if (nome.includes(searchTerm) || searchTerm === "") {
+        // Se corresponder (ou se a pesquisa estiver vazia), exibe o elemento
+        element.style.display = "flex";
+      } else {
+        // Caso contrário, oculta o elemento
+        element.style.display = "none";
+      }
+    }
+  });
 };
 
 // Ouça o evento 'input' na barra de pesquisa
 searchBar.addEventListener("input", debounce(search, 1000));
 
+searchBar.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    search();
+  }
+});
